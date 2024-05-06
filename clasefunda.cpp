1fa58996991f0fe7787711e0c6b6006ff9051f54
00007024 <164571431+00007024@users.noreply.github.com>
2024-05-06 12:55:55 -0600

    clase

---
+++ clasefunda.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n=0;
+    int i=1;
+    cout << "ingrese un numero entero positivo";
+    cin >> n; 
+    while (i<n)
+{
+cout << i << "\n" ;
+i = i+1 ;
+}
+
+
+}
---
+++ tabla.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i=0;
+    int n=1;
+    cout  << "ingrese un numero entero positivo \n" ;
+    cin >> n;
+    while (i<10)
+    {
+        cout << i << "\n" ;
+        i = i+1 ;
+    }
+}
+
